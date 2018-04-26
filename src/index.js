f91421a07a462c37c1355a381dce61899d18bb5e
baihua@sndskj.com <baihua2018>
2018-04-26 17:42:26 +0800

    project base run

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import 'babel-polyfill';
+import React from 'react';
+import {render} from 'react-dom';
+import {Provider} from 'redux';
+// import todoApp from './reducers';
+// import App from './components/App';
+
+render(
+    <div></div>,
+    document.getElementById('root')
+);
